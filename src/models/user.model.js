202790fab8f4a5245fab78a1daa85f9338bd946d
Muhammad-Zakria-Zaheer <muhammadzakria977@gmail.com>
2024-12-31 12:15:03 +0500

    added controller for registeration

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,7 +56,7 @@ const userSchema = new Schema({
 userSchema.pre("save", async function(next){
     // const salt = await bcrypt.genSalt(10)
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password =await  bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -90,4 +90,4 @@ userSchema.methods.generateRefreshToken = function(){
 )
 }
 
-export const UserModel = mongoose.model("User", UserSchema)
+export const User = mongoose.model("User", userSchema)
